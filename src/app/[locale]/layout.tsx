e08bd7caf6cfc9f5f644078c492b065afafe016d
Marc Got <gotcritgmarc@gmail.com>
2026-03-28 16:48:52 +0100

    feat: add Header component for improved navigation and user experience

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import { ReactNode } from "react";
 import { ViewTransitions } from "next-view-transitions";
 import { getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
+import Header from "@/components/layout/Header/Header";
 
 export const metadata: Metadata = {
     title: "Dulces Pétalos",
@@ -23,16 +24,17 @@ export default async function RootLayout({
 
     return (
         <ViewTransitions>
-            <html
-                lang={locale}
-                className={`${nunitoSans.variable} ${dmSans.variable} bg-background h-full antialiased`}
-            >
-                <body className="m-auto flex min-h-full max-w-360 flex-col px-12">
-                    <NextIntlClientProvider messages={messages} locale={locale}>
+            <NextIntlClientProvider messages={messages} locale={locale}>
+                <html
+                    lang={locale}
+                    className={`${nunitoSans.variable} ${dmSans.variable} bg-background h-full antialiased`}
+                >
+                    <body className="m-auto flex min-h-full max-w-360 flex-col px-12 pt-20">
+                        <Header />
                         {children}
-                    </NextIntlClientProvider>
-                </body>
-            </html>
+                    </body>
+                </html>
+            </NextIntlClientProvider>
         </ViewTransitions>
     );
 }
