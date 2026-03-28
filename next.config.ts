088b4631996b88e9b14bae602734deadfb794b8f
Marc Got <gotcritgmarc@gmail.com>
2026-03-28 16:18:52 +0100

    initialize Next.js project with essential configurations and dependencies

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dulces-petalos.jakala.es',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
+};
+
+export default withNextIntl(nextConfig);
